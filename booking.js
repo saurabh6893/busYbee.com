5857bd61e801d8c631bde49c6e90f95ab61dc071
saurabh <saurabhkedar@gmail.com>
2022-02-03 06:21:45 +0530

    phase 3 completed

--- booking.js
+++ booking.js
@@ -11,6 +11,24 @@ const pickup = document.getElementById('location')
 const destination = document.getElementById('destination')
 const ticketSet = 10
 let cost
+feedToUi()
+function feedToUi() {
+  const selectedseats = JSON.parse(localStorage.getItem('selectedseats'))
+
+  if (selectedseats.length !== null && selectedseats.length > 0) {
+    seat.forEach((seats, index) => {
+      if (selectedseats.indexOf(index) > -1) {
+        seats.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedDestinationIndex = localStorage.getItem('selectedDestIndex')
+
+  if (selectedDestinationIndex !== null) {
+    mainBox.selectedIndex = selectedDestinationIndex
+  }
+}
 
 //function Declarations
 function ticketCost() {
@@ -23,11 +41,23 @@ function ticketCost() {
   return +cost
 }
 
+function busBookinData(destIndex, destinationCost) {
+  localStorage.setItem('selectedDestIndex', destIndex)
+  localStorage.setItem('destinationCost', destinationCost)
+}
+
 function update() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatNo = [...selectedSeats].map((seats) => [...seat].indexOf(seats))
+  localStorage.setItem('selectedseats', JSON.stringify(seatNo))
   const noSelectedSeats = selectedSeats.length
+
   count.innerText = noSelectedSeats
   total.innerText = ticketCost() * noSelectedSeats
+  if (pickup.value > destination.value) {
+    noSelectedSeats = 0
+  }
 }
 
 //event listeners
@@ -41,4 +71,9 @@ boggie.addEventListener('click', (e) => {
   update()
 })
 
-mainBox.addEventListener('change', () => update())
+mainBox.addEventListener('change', (e) => {
+  busBookinData(e.target.selectedIndex, e.target.value)
+  update()
+})
+
+update()
